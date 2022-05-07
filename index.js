60326afdd71589ba8371ebac9169a8f04c20745a
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-05-07 11:48:17 +0600

    gitignore

--- index.js
+++ index.js
@@ -46,6 +46,12 @@ async function run() {
       const result = await fruitCollection.insertOne(newItem);
       res.send(result);
     });
+    // app.delete("/fruit/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: ObjectId(id) };
+    //   const result = await fruitCollection.deleteOne(query);
+    //   res.send(result);
+    // });
   } finally {
   }
 }
